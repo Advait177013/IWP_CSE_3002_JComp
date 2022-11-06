7cca674fe6a4871a4e2f1afdd888c340cee8a3eb
Advait177013 <72642229+Advait177013@users.noreply.github.com>
2022-11-06 20:37:02 +0530

    Removed Password

--- Proj Code/mailingtest.php
+++ Proj Code/mailingtest.php
@@ -13,7 +13,7 @@ try {
 	$mail->Host	 = 'smtp.gmail.com;';					
 	$mail->SMTPAuth = true;							
 	$mail->Username = 'advaitdeochakke@gmail.com';				
-	$mail->Password = 'btdkwzhvcktacfao'; //appspecific password generation for automation						
+	$mail->Password = '<Password Removed>'; //appspecific password generation for automation						
 	$mail->SMTPSecure = 'tls';							
 	$mail->Port	 = 587;
 
